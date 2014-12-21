12680294749901b746627396fb36664b1ed3d7a3
Michael Ferreira <carvalhoferreira.michael@gmail.com>
2014-12-21 02:40:31 +0000

    First Commit

---
+++ PlanetAid/PlanetAid/Entities/Flask.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetAid.Entities
+{
+    class Flask
+    {
+        private Vector2 velocity;
+        private Vector2 position;
+        private Texture2D flaskImg;
+
+
+       public Flask(Vector2 vel, Vector2 pos, Texture2D img)
+        {
+            //vel.Normalize();
+            velocity = vel;
+            position = pos;
+            flaskImg = img;
+        }
+       public void update()
+       {
+
+           position += new Vector2(20, 0);
+       }
+
+       public void Draw(SpriteBatch sb)
+       {
+           sb.Draw(flaskImg, new Rectangle((int)position.X, (int)position.Y, flaskImg.Width/8, flaskImg.Height/8), Color.White);
+
+       }
+
+    
+    }
+}
---
+++ PlanetAid/PlanetAid/Game1.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using PlanetAid.Entities;
+
+namespace PlanetAid
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Texture2D background;
+        private Texture2D railgun;
+        private Texture2D flaskImg;
+        private float gunRotation;
+        Flask flask; 
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1280;
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = Content.Load<Texture2D>("background");
+            railgun = Content.Load<Texture2D>("railgun");
+            flaskImg = Content.Load<Texture2D>("Flask");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+          //Railgun rotation and limitations
+            gunRotation = (float)Math.Atan2(Mouse.GetState().Y - 220, Mouse.GetState().X - 10);
+            if (gunRotation <= -.523)
+                gunRotation = -.523f;
+
+            if (gunRotation >= .174)
+                gunRotation = .174f;
+
+            IsMouseVisible = true;
+
+
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+               flask = new Flask(new Vector2(Mouse.GetState().Y - 220, Mouse.GetState().X - 10),
+                    new Vector2(170, 170),
+                    flaskImg);
+            }
+            if (flask != null) flask.update();
+
+
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+            spriteBatch.Draw(railgun, new Rectangle(70, 240, railgun.Width / 4, railgun.Height / 4), null, Color.White,gunRotation, new Vector2(195, 235), SpriteEffects.None, 0);
+            if (flask != null) flask.Draw(spriteBatch);
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
